7cb0d0a2131fd92bab5986a542e18e31bf437e72
Emad <emadi68moh@gmail.com>
2022-10-24 19:52:24 +0330

    init test boilerplate

---
+++ src/my-elements.test.ts
@@ -0,0 +1,6 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import './my-element';
+
+describe('add some description', async () => {
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   build: {
     lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
+      entry: "src/my-element.ts",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: /^lit/
-    }
-  }
-})
+      external: /^lit/,
+    },
+  },
+});
