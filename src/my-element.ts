5708edb2c2b2643cb128ab0a358a8c4264feaca0
Emad <emadi68moh@gmail.com>
2022-10-25 18:59:32 +0330

    structure change
    moving dayjs to date component

--- src/my-element.ts
+++ src/my-element.ts
@@ -3,50 +3,33 @@ import { customElement, property } from "lit/decorators.js";
 
 import "./date/xox-date";
 
-import d from "dayjs";
-import dj from "./j.js";
-import localeDate from "dayjs/plugin/localeData";
-import weekday from "dayjs/plugin/weekday";
-import isoWeek from "dayjs/plugin/isoWeek";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
-import "dayjs/locale/en-au";
-
 @customElement("my-element")
 export class MyElement extends LitElement {
   @property({ type: String, attribute: true })
-  locale: string = "en";
+  locale: string = "fa";
+
+  @property({ type: Number, attribute: true })
+  timestamp: number = 0;
+
+  @property({ type: String, attribute: true })
+  value: string = "";
+
+  // HANDLERS
+  _update = (e:CustomEvent) => { 
+    const d = e.detail;
+    if (d) { 
+      console.log(d)
+    }
+  }
 
   render() {
-    d.extend(localeDate);
-    d.extend(dj);
-    d.extend(weekday);
-    d.extend(isoWeek);
-    d.extend(utc);
-    d.extend(timezone);
-
-    const _d = d("2022/01/02", "YYYY/MM/DD")
-      .locale("en")
-      .tz("Asia/Tehran", true);
-    // const _d = d("2022/01/02", "YYYY/MM/DD")
-    //   .locale("en")
-    //   .tz("America/New_York", true);
-
-    const globalLocaleData = _d.localeData();
-
-    // console.log(_d.daysInMonth());
-
-    // console.log(_d.isoWeek());
-    console.log(_d.utc().format('d'));
-
     return html`
       <h1>hello</h1>
-
       <xox-date
-        months=${globalLocaleData.months().join("_")}
-        year=${_d.format("YYYY")}
-        numberOfDaysInMonth=${_d.daysInMonth()}
+        locale=${this.locale}
+        timestamp=${this.timestamp}
+        value=${this.value}
+        @change=${this._update}
       ></xox-date>
     `;
   }
